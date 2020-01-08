f1ce0d2fdc15ed7bc2182e974c410875f170fe9a
jstiven01 <stiven10@gmail.com>
2020-01-08 16:37:10 -0500

    Adding TDD with calculator

---
+++ src/calculator.js
@@ -0,0 +1,16 @@
+const calculator = (() => {
+    const add = (number1, number2) => number1 + number2
+    const subtract = (number1, number2) => number1 - number2
+    const multiply = (number1, number2) => number1 * number2
+    const divide = (number1, number2) => {
+        if (number2 === 0 ) throw new Error('You are dividing by 0');
+        return number1 / number2
+    }
+
+    return {
+        add, subtract, multiply, divide
+    }
+
+})();
+
+export default calculator
---
+++ test/calculator.test.js
@@ -0,0 +1,28 @@
+import calculator from '../src/calculator'
+
+test('adding two numbers', () => {
+    const result = calculator.add(4,5);
+    expect(result).toBe(9);
+});
+
+test('subtracting two numbers', () => {
+    const result = calculator.subtract(9,3);
+    expect(result).toBe(6);
+});
+
+test('dividing two numbers', () => {
+    const result = calculator.divide(8,2);
+    expect(result).toBe(4);
+});
+
+test('dividing by 0', () => {
+    const divisionBy0 = () => {
+        calculator.divide(4, 0);
+    }
+    expect(divisionBy0).toThrow('You are dividing by 0')
+});
+
+test('multiplying two numbers', () => {
+    const result = calculator.multiply(4, -3);
+    expect(result).toBe(-12);
+})
